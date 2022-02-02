39018ddc1b6f1de939b50ad234998b7df392f3a2
jasonlin1222 <jasonlin1222@gmail.com>
2022-02-02 15:06:19 +0800

    add upload

--- sf-backend/app.py
+++ sf-backend/app.py
@@ -1,11 +1,22 @@
+import os
 from flask import Flask, json, jsonify, request
 from flask_cors import CORS, cross_origin
-import os
+from werkzeug.utils import secure_filename
 
+# config
 app = Flask(__name__)
+UPLOAD_FOLDER='/home/jason/Desktop/SF_image_app/sf-backend/image'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['FLASK_ENV'] = deve
+# helper function
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# app
 @app.route("/")
 def index():
     return "This is the api for sf-image-app"
@@ -14,3 +25,31 @@ def index():
 def query():
     body = request.args.get('query', "dog")
     return jsonify({'success':1, 'image_url':body})
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
